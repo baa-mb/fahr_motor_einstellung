c98f399262cd6466a1aecad49a26d49e518ec4cf
baa <34665120+baa-mb@users.noreply.github.com>
2026-06-12 20:05:55 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,101 @@
+radio.onReceivedValue(function (info, wert) {
+    if (info == "kurve") {
+        kurve_get = wert
+        kurve_rad = Math.round(Math.map(kurve_get, -45, 45, -255, 255))
+    } else if (info == "gerade") {
+        gerade_get = wert
+        gerade_rad = Math.round(Math.map(gerade_get, -45, 45, -255, 255))
+    } else if (info == "kupplung") {
+        if (wert == 0) {
+            robotbit.Servo(robotbit.Servos.S1, 0)
+        } else {
+            robotbit.Servo(robotbit.Servos.S1, hebe_winkel)
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (oben) {
+        robotbit.Servo(robotbit.Servos.S1, 0)
+    } else {
+        robotbit.Servo(robotbit.Servos.S1, hebe_winkel)
+    }
+    oben = !(oben)
+})
+function startimpuls () {
+    robotbit.MotorRunDual(
+    robotbit.Motors.M1A,
+    start_speed,
+    robotbit.Motors.M2A,
+    start_speed
+    )
+    basic.pause(100)
+}
+let speed = 0
+let oben = false
+let gerade_rad = 0
+let gerade_get = 0
+let kurve_rad = 0
+let kurve_get = 0
+let start_speed = 0
+let hebe_winkel = 0
+let abstand = 0
+let rechts_ist = 0
+let links_ist = 0
+let rechts_rad = 0
+let links_rad = 0
+let lauf_flag = 0
+radio.setGroup(26)
+hebe_winkel = 70
+let motor_links = robotbit.Motors.M1A
+let motor_rechts = robotbit.Motors.M2A
+robotbit.MotorStopAll()
+start_speed = 120
+let nomove_value = 5
+let limit = 12
+let diff = 10
+let zzz = nomove_value
+startimpuls()
+basic.forever(function () {
+    led.plot(zzz % 5, 4 - Math.floor(zzz / 5))
+    if (zzz >= limit) {
+        zzz = nomove_value
+        basic.showIcon(IconNames.Heart)
+        startimpuls()
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+        zzz += 1
+    }
+    speed = zzz * diff
+    serial.writeValue("x", speed)
+    robotbit.MotorRunDual(
+    robotbit.Motors.M1A,
+    speed,
+    robotbit.Motors.M2A,
+    speed
+    )
+    basic.pause(2000)
+})
+// while (true) {
+// let strip: neopixel.Strip = null
+// abstand = sonar.ping(
+// DigitalPin.P1,
+// DigitalPin.P2,
+// PingUnit.Centimeters
+// )
+// led.plotBarGraph(
+// abstand,
+// 100
+// )
+// strip.rotate(1)
+// strip.show()
+// basic.pause(2000)
+// }
+control.inBackground(function () {
+	
+})
