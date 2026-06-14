3e8aad412439d5679b5ff1a7840085c86049f58d
baa <34665120+baa-mb@users.noreply.github.com>
2026-06-14 13:15:42 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,14 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    zzz += receivedNumber
+    basic.clearScreen()
     if (receivedNumber == 2) {
         zzz = zzz_vorgabe
-        rad_faktor = rad_faktor + 0.05
+        rad_faktor = rad_faktor * 1
     }
+    if (receivedNumber == -1) {
+        zzz_vorgabe = zzz_vorgabe - 1
+        rad_faktor = rad_faktor * 1
+    }
+    zzz += receivedNumber
 })
 input.onButtonPressed(Button.B, function () {
     richtung = richtung * -1
@@ -22,6 +20,12 @@ function startimpuls (v: number) {
     robotbit.Motors.M2A,
     v * richtung * rad_faktor
     )
+    robotbit.MotorRunDual(
+    robotbit.Motors.M1B,
+    v * richtung,
+    robotbit.Motors.M2B,
+    v * richtung * rad_faktor
+    )
 }
 let speed = 0
 let rad_faktor = 0
@@ -31,15 +35,15 @@ let richtung = 0
 robotbit.MotorStopAll()
 basic.pause(1000)
 richtung = 1
-zzz_vorgabe = 4
+zzz_vorgabe = 5
 zzz = zzz_vorgabe
-let start_speed = 110
+let start_speed = 100
 let step_size = 10
-rad_faktor = 0.95
-let limit = 14
+rad_faktor = 1
+let limit = 16
 startimpuls(start_speed)
 basic.forever(function () {
-    led.plot(zzz % 5, 4 - Math.floor(zzz / 5))
+    basic.showNumber(zzz % 10)
     // zzz += 1
     if (zzz >= limit) {
         zzz = zzz_vorgabe
@@ -56,25 +60,6 @@ basic.forever(function () {
     	
     }
     speed = zzz * step_size * richtung
-    serial.writeValue("speed", speed)
     startimpuls(speed)
-    basic.pause(2000)
-})
-// while (true) {
-// let strip: neopixel.Strip = null
-// abstand = sonar.ping(
-// DigitalPin.P1,
-// DigitalPin.P2,
-// PingUnit.Centimeters
-// )
-// led.plotBarGraph(
-// abstand,
-// 100
-// )
-// strip.rotate(1)
-// strip.show()
-// basic.pause(2000)
-// }
-control.inBackground(function () {
-	
+    basic.pause(1000)
 })
