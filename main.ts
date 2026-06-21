998a40689b53633fb26b1c6aa078fe20085447d8
baa <34665120+baa-mb@users.noreply.github.com>
2026-06-21 13:01:10 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,89 @@
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    init()
+    logotouch()
 })
-// })
+function logotouch() {
+    robotbit.MotorStopAll()
+    basic.showNumber(rad_faktor)
+    autolauf = 0
+    basic.pause(500)
+    zzz = zzz_vorgabe
+
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . . . . .
+        . . . . .
+        `)
+    let v_arr = "7,8,9,10,11,12,13,14,15,16,15,10,9,8,7,6,5,4,3"
+        .split(",")
+        .map(s => parseInt(s))
+
+    for (let index = 0; index < 3; index++) {
+        for (let index = 0; index <= v_arr.length - 1; index++) {
+            wert = v_arr[index]
+            basic.showNumber(wert % 10)
+            zzz = wert
+            // dauerschleife(1)
+            drehimpuls(10 * zzz)
+            basic.pause(500)
+        }
+        robotbit.MotorStopAll()
+    }
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+}
 input.onButtonPressed(Button.A, function () {
-    // radio.sendNumber(-1)
-    werte_rechnen(-1)
+    rad_faktor += -0.1
+    basic.showNumber(rad_faktor)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
 })
-function init () {
+
+input.onButtonPressed(Button.B, function () {
+    rad_faktor += 0.1
+    basic.showNumber(rad_faktor)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+})
+function drehimpuls(v: number) {
+    robotbit.MotorRunDual(
+        robotbit.Motors.M1A,
+        v * richtung,
+        robotbit.Motors.M2A,
+        v * richtung * rad_faktor
+    )
+    robotbit.MotorRunDual(
+        robotbit.Motors.M1B,
+        v * richtung,
+        robotbit.Motors.M2B,
+        v * richtung * rad_faktor
+    )
+}
+function init() {
+    zzz_vorgabe = 5
+    zzz = zzz_vorgabe
     start_speed = 110
     step_size = 10
     rad_faktor = 1
     limit = 14
-    startimpuls(start_speed)
+    // drehimpuls(start_speed)
     basic.showIcon(IconNames.SmallHeart)
+    robotbit.MotorStopAll()
+    // basic.showNumber(rad_faktor)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    logotouch()
 }
-function werte_rechnen (receivedNumber: number) {
+function werte_rechnen(receivedNumber: number) {
     basic.clearScreen()
     if (receivedNumber == 2) {
         zzz = zzz_vorgabe
@@ -31,50 +100,40 @@ input.onButtonPressed(Button.AB, function () {
     zzz = zzz_vorgabe
     init()
 })
-input.onButtonPressed(Button.B, function () {
-    // radio.sendNumber(-1)
-    werte_rechnen(1)
-})
-function startimpuls (v: number) {
-    robotbit.MotorRunDual(
-    robotbit.Motors.M1A,
-    v * richtung,
-    robotbit.Motors.M2A,
-    v * richtung * rad_faktor
-    )
-    robotbit.MotorRunDual(
-    robotbit.Motors.M1B,
-    v * richtung,
-    robotbit.Motors.M2B,
-    v * richtung * rad_faktor
-    )
-}
-let speed = 0
-let limit = 0
-let rad_faktor = 0
-let step_size = 0
-let start_speed = 0
-let zzz = 0
-let zzz_vorgabe = 0
-let richtung = 0
-robotbit.MotorStopAll()
-basic.pause(1000)
-richtung = 1
-zzz_vorgabe = 4
-zzz = zzz_vorgabe
-init()
-basic.forever(function () {
+
+function dauerschleife(warte: number) {
     basic.showNumber(zzz % 10)
     // zzz += 1
     if (zzz >= limit) {
         zzz = zzz_vorgabe
         basic.showIcon(IconNames.Heart)
-        startimpuls(start_speed)
+        drehimpuls(start_speed)
         basic.clearScreen()
     } else {
-    	
+
     }
     speed = zzz * step_size * richtung
-    startimpuls(speed)
-    basic.pause(1000)
+    drehimpuls(speed)
+    basic.pause(warte)
+}
+let speed = 0
+let limit = 0
+let step_size = 0
+let start_speed = 0
+let wert = 0
+let v_arr: number[] = []
+let zzz_vorgabe = 0
+let zzz = 0
+let rad_faktor = 0
+let autolauf = 0
+let richtung = 0
+robotbit.MotorStopAll()
+basic.pause(1000)
+richtung = 1
+autolauf = 0
+init()
+basic.forever(function () {
+    if (autolauf == 1) {
+        dauerschleife(1000)
+    }
 })
